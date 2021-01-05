0acd8943ad827365c6eb4d50c14943522bc9063c
Murilo S Rodrigues Jr <murilo.cc.uepb@gmail.com>
2021-01-04 23:17:25 -0300

    Create Tab2 and add remove notification feature

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  listOfNotifications: any[] = [
+    {
+      "id": 1234,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "sender": "Pedro Alcantara",
+      "message": "Pagamento Realizado"
+    },
+    {
+      "id": 1235,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "sender": "Rita Fonseca",
+      "message": "Pagamento Realizado"
+    },
+    {
+      "id": 1236,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "sender": "Andersson Emmanuel",
+      "message": "Pagamento Realizado"
+    }
+  ];
+  constructor() { }
 
-  constructor() {}
+  deleteItem(itemId: number) {
+    this.listOfNotifications.splice(this.findIndexByItemId(itemId),1);
+  }
+
+  findIndexByItemId(itemId: number):number {
+    let indexItem=-1;
+    this.listOfNotifications.forEach(item => {
+     if(itemId===item.id) indexItem = this.listOfNotifications.indexOf(item);
+    });
+    return indexItem;
+  }
 
 }
