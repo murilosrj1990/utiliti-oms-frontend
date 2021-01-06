dd900a1ac9045e098f7be3e584bb762909ba3f86
Murilo S. Rodrigues Jr <murilo.cc.uepb@gmail.com>
2021-01-05 21:39:27 -0300

    Implements Search Bar animation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
+  isOpenedSearchBar=false;
+  listOfClients: any[] = [
+    {
+      "id": 1234,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "name": "Pedro Alcantara",
+      "phone": "(83) 99999-9999"
+    },
+    {
+      "id": 1235,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "name": "Rita Fonseca",
+      "phone": "(83) 99999-9999"
+    },
+    {
+      "id": 1236,
+      "img_src": "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y",
+      "name": "Andersson Emmanuel",
+      "phone": "(83) 99999-9999"
+    }
+  ];
   constructor() {}
 
 }
